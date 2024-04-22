bd001658e381b2a0582a71e75dd0f8018469151c
Your Name <you@example.com>
2024-04-22 13:34:05 +0530

    my

---
+++ opps.py
@@ -0,0 +1,8 @@
+#opps concept
+#1.class.
+#2.object.
+#3.method.
+#4.inheritance.
+#5.polymorphism.
+#6Data Abstraction.
+#Encapsulation.
---
+++ shaps.py
@@ -0,0 +1,8 @@
+for i in range(1,6):
+	{ 
+		for j in range(i,i-1,1):
+			{
+				print(i,end="")
+			}
+		print()
+	}
